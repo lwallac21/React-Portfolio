f6f95827ad37ce22b8ba95a425320b95381126f8
Lawrence Wallace <lwallac21@gmail.com>
2021-02-11 16:25:29 -0500

    nav-bar

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import About from './components/About'
 import SinglePost from './components/SinglePost'
 import Post from './components/Post'
 import Projects from './components/Projects'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <BrowserRouter>
-
+      <Nav />
       <Switch>
         <Route exact path='/' component={Main} />
         <Route path='/about' component={About} />
         <Route path='/post/:slug' component={SinglePost} />
         <Route path='/post' component={Post} />
-        <Route path='/project' component={Projects} />
+        <Route path='/projects' component={Projects} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,19 +4,31 @@ import {NavLink} from 'react-router-dom'
 
 export default function Nav() {
     return (
-        <header>
-            <div>
-                <nav>
-                    <NavLink exact to='/'>
+        <header className='bg-blue-600'>
+            <div className='container m-auto flex justify-between'>
+                <nav className="flex">
+                    <NavLink 
+                        className="inflex-flex tracking-widest items-center py-5 px-3 mr-4 text-blue-100 hover:text-yellow-600 text-2xl font-bold styled-text" 
+                        exact to='/'
+                    >
                         Lawrence Wallace
                     </NavLink>
-                    <NavLink to='post'>
+                    <NavLink 
+                        className="inline-flex items-center py-3 py-3 my-6 rounded text-blue-200 hover:text-yellow-500"
+                        to='post'
+                    >
                         Updates
                     </NavLink>
-                    <NavLink to="/projects">
+                    <NavLink 
+                        className="inline-flex items-center py-3 py-3 my-6 rounded text-blue-200 hover:text-yellow-500"
+                        to="/projects"
+                    >
                         Projects
                     </NavLink>
-                    <NavLink to='/about'>
+                    <NavLink 
+                        className="inline-flex items-center py-3 py-3 my-6 rounded text-blue-200 hover:text-yellow-500"
+                        to='/about'
+                    >
                         About
                     </NavLink>
                 </nav>
